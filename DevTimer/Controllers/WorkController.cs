073495b09dd7ea1bffffd57f0eee4ce238432646
kwingat <kwingat@gmail.com>
2015-10-13 16:52:46 -0400

    Started Adding "Work"

---
+++ DevTimer/Controllers/WorkController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using DevTimer.Domain.Abstract;
+
+namespace DevTimer.Controllers
+{
+    public class WorkController : Controller
+    {
+        private readonly IAspNetUserRepository _aspNetUserRepository;
+        private readonly IClientRepository _clientRepository;
+        private readonly IProjectRepository _projectRepository;
+        private readonly IWorkRepository _workRepository;
+        private readonly IWorkTypeRepository _workTypeRepository;
+
+        public WorkController(
+            IAspNetUserRepository aspNetUserRepository,
+            IClientRepository clientRepository,
+            IProjectRepository projectRepository,
+            IWorkRepository workRepository,
+            IWorkTypeRepository workTypeRepository)
+        {
+            _aspNetUserRepository = aspNetUserRepository;
+            _clientRepository = clientRepository;
+            _projectRepository = projectRepository;
+            _workRepository = workRepository;
+            _workTypeRepository = workTypeRepository;
+
+        }
+
+        // GET: Work
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ DevTimer/Models/WorkListViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using AutoMapper;
+using DevTimer.Core;
+using DevTimer.Domain.Entities;
+
+namespace DevTimer.Models
+{
+    public class WorkListViewModel : IHasCustomMapping
+    {
+        public int ID { get; set; }
+
+        public int ProjectID { get; set; }
+
+        public int WorkTypeID { get; set; }
+
+        public string UserID { get; set; }
+
+        public TimeSpan? StartTime { get; set; }
+
+        public TimeSpan? EndTime { get; set; }
+
+        public decimal? Hours { get; set; }
+
+        public string AspNetUser { get; set; }
+
+        public string Project { get; set; }
+
+        public string WorkType { get; set; }
+
+        public void CreateMapping(IConfiguration configuration)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
