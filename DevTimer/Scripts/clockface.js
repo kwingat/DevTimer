84c27f91fb67b691f5817fc6e6ff8c8425a4c54f
kwingat <kwingat@gmail.com>
2015-12-18 10:37:22 -0500

    fixed time edit/create modal

--- DevTimer/Scripts/clockface.js
+++ DevTimer/Scripts/clockface.js
@@ -342,7 +342,7 @@
         place: function(){
           var zIndex = parseInt(this.$element.parents().filter(function() {
                    return $(this).css('z-index') != 'auto';
-             }).first().css('z-index'), 10)+10,
+             }).first().css('z-index'), 10)+99999,
              offset = this.$element.offset();
           this.$clockface.css({
             top: offset.top + this.$element.outerHeight(),
